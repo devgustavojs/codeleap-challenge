f2b1faffd514207c6773326bc01a2dbf518ee6cb
Gustavo Gomes <gustsgomesbr@hotmail.com>
2021-08-08 15:55:06 -0400

    useButton Hook

---
+++ src/hooks/useButton.ts
@@ -0,0 +1,43 @@
+import { useState } from "react";
+
+export function useButton(NumberOfInputs: number){
+  const [inputEnteredValue, setInputEnteredValue] = useState('');
+  const [textareaEnteredValue, setTextareaEnteredValue] = useState('');
+
+  let isActive = false;
+
+  if(NumberOfInputs === 1){
+    if(inputEnteredValue){
+      isActive = true;
+    }else{
+      isActive = false;
+    }
+  }else if(NumberOfInputs === 2){
+
+    if(inputEnteredValue && textareaEnteredValue){
+      isActive = true;
+    }else{
+      isActive = false;
+    }
+  }
+
+  // TO DO: RETURN WHAT NEED TO BE FILLED TO BUTTON BE ACTIVE.
+
+  return {
+    isActive,
+    inputEnteredValue,
+    textareaEnteredValue,
+    input: {
+      onChange: (event: React.ChangeEvent<HTMLInputElement>) =>{
+        setInputEnteredValue(event.target.value)
+      }
+    },
+    textarea: {
+      onChange: (event: React.ChangeEvent<HTMLTextAreaElement>) =>{
+        setTextareaEnteredValue(event.target.value)
+      }
+    }
+
+  }
+
+}
