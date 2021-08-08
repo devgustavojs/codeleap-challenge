c7156972889bd9a2c116c6d4c9c1be961bd8aa0c
Gustavo Gomes <gustsgomesbr@hotmail.com>
2021-08-08 15:52:41 -0400

    Component button updated

--- src/components/Button/index.tsx
+++ src/components/Button/index.tsx
@@ -1,15 +1,15 @@
 import './styles.scss'
-
-
 interface ButtonProps {
   value: string,
+  isActive: boolean,
+  onClick?: | ((event: React.MouseEvent<HTMLButtonElement, MouseEvent>) => void)
+  | undefined;
 }
 
-export function Button({value, ...rest}: ButtonProps){
+export function Button({value, isActive, ...rest}: ButtonProps){
 
   return(
-    <button>{value}</button>
-
+    <button className="componentButton" disabled={!isActive} {...rest}>{value}</button>
   )
 
 }
